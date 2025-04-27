f7cafad1803bac962173ccaca324920c8ddf0295
dylan.bickerstaff <dylan@belowaverage.org>
2025-04-27 00:13:47 -0400

    .

--- app/src/main/java/com/plogertrans/trucker/launcher/MainActivity.kt
+++ app/src/main/java/com/plogertrans/trucker/launcher/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.plogertrans.trucker.launcher
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.LauncherApps
 import android.os.Bundle
+import android.os.Process
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +23,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val listLayout1 = findViewById<LinearLayout>(R.id.listLayout1)
+        val button1 = findViewById<Button>(R.id.button1)
+
+        val intent = packageManager.getLaunchIntentForPackage("com.android.chrome")
+
+        button1.setOnClickListener {
+
+            startActivity(intent)
+
+        }
+
     }
 }
